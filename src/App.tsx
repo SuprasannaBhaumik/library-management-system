f31ed5f438776cc4a79bd994db25b413d93603b9
Suprasanna Bhaumik <suprasanna.bhaumik86@gmail.com>
2019-06-11 15:21:50 +0530

    with logged in users redirected to landing page on successful login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
+
 import './App.css';
 
 import Login from './login/view/Login';
-
 import GoogleLogin from 'react-google-login';
-import FacebookLogin from 'react-facebook-login';
+// import FacebookLogin from 'react-facebook-login';
 
 import LMSHeader  from './application/view/LMSHeader';
 import LMSFooter  from './application/view/LMSFooter';
-import Form from 'react-bootstrap/Form';
+import Home from './application/view/Home';
 
+import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { Redirect, Route, Switch } from 'react-router-dom';
+
 interface InternalState {
     username: string;
+    imgUrl: string;
+    loggedIn: boolean;
 }
     
-interface Props {
-
+interface Props{
+    // history: any;
 }
 
 class App extends React.Component<Props, InternalState> {
@@ -29,12 +34,15 @@ class App extends React.Component<Props, InternalState> {
         super(props);
         
         this.state ={
-            username: ''
+            username: '',
+            imgUrl: '',
+            loggedIn: false
         }
         
         this.responseGoogle = this.responseGoogle.bind(this);
         this.responseFacebook = this.responseFacebook.bind(this);
         this.failureGoogle = this.failureGoogle.bind(this);
+        this.navigateToHome = this.navigateToHome.bind(this);
     }
 
 
@@ -42,7 +50,7 @@ class App extends React.Component<Props, InternalState> {
     public render() {
         return (
             <div className="App">
-                <LMSHeader username={this.state.username}/>
+                <LMSHeader username={this.state.username} imgUrl={this.state.imgUrl}/>
                 {this.state.username === '' && 
                 <div className="encloser">
                     <Login/>
@@ -74,9 +82,10 @@ class App extends React.Component<Props, InternalState> {
 
                 </div>
                 }
-                <div className="content">
-                    <span>Dummy content</span>
-                </div>
+                {this.state.loggedIn ? <Redirect to="/home" />: null}
+                <Switch>
+                    <Route path='/home' exact component={Home}/>
+                </Switch>
                 <LMSFooter/>
             </div>
         );
@@ -84,18 +93,26 @@ class App extends React.Component<Props, InternalState> {
 
     public responseGoogle(response: any){
         this.setState({
-            username: response.w3.ofa
+            username: response.profileObj.givenName,
+            imgUrl: response.profileObj.imageUrl,
+            loggedIn: true
         });
+        // , () => this.navigateToHome()
     }
 
     public responseFacebook(response: any) {
         this.setState({
-            username: response.w3.ofa
+            username: response.profileObj.givenName,
+            imgUrl: ''
         });
     }
 
     public failureGoogle(response: any){
         // login failed
+    }
+
+    public navigateToHome() {
+        return <Redirect to="/home" />
     }
 
 }
